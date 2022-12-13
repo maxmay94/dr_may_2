55626020700bc0b4c0f21967c86fd3b03b2902a8
maxmay94 <maxmay94@gmail.com>
2022-12-13 14:57:01 -0500

    add infor sections

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="flex justify-around bg-slate-200/80 backdrop-blur-md shadow-md w-full fixed top-0 left-0 right-0 z-10">
+    <div className="flex justify-around bg-slate-200/80 mix-blend-luminosity backdrop-blur-md shadow-md w-full fixed top-0 left-0 right-0 z-10">
       <div className='font-sans w-screen'>
         <nav className="">
           <ul className="flex flex-row">
