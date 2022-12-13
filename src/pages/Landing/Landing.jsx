8fbbc7392cbf02051242abe1dba64bd690af65ab
maxmay94 <maxmay94@gmail.com>
2022-12-13 13:01:42 -0500

    messing with image filters

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,9 +2,16 @@ const Landing = () => {
   return (
     <main className="relative w-screen h-full">
       <div className="lg:w-2/3 mx-auto">
-        <div className="">
-          
+
+        <div className="relative">
+          <img className="saturate-[.8]" src="src/images/happy_people_temp.jpeg" alt="happy_people_temp" />
+          <div className="absolute bottom-1">
+            <p className="font-bold text-3xl rounded-xl backdrop-blur-[3px] bg-slate-900/30 p-3">
+              Tired of back pain  and <br /> some other stuff <br /> ruining you <br /> life  all the time?
+            </p>
+          </div>
         </div>
+
       </div>
     </main>
   )
