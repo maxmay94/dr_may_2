2a8a5ff72038cd5606bdfec10bde47e08e82a3fb
maxmay94 <maxmay94@gmail.com>
2022-12-13 23:52:43 -0500

    remove pink, oops

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,20 +7,20 @@ const Footer = () => {
         <nav className="">
           <ul className="flex flex-row">
 
-            <li className="grow m-2 text-xs font-light bg-red-200">
+            <li className="grow m-2 text-xs font-light">
               <p className="text-slate-900">
                 docmay@gmail.com
               </p>
             </li>
 
-            <li className="grow sm:m-2 m-0 bg-red-200">
+            <li className="grow sm:m-2 m-0">
               <div className="text-xs text-sky-900 sm:block hidden">
                 73 Church Street, North Adams, MA 01247
               </div>
             </li>
 
             <li className="m-2 text-xs font-light">
-              <p className="text-slate-900 bg-red-200">
+              <p className="text-slate-900">
                 (413) 664-9050
               </p>
             </li>
