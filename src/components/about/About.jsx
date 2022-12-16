cae16e245178a6f72bdc5583f0251bb695e36b58
maxmay94 <maxmay94@gmail.com>
2022-12-16 13:00:36 -0500

    add new dad pic, adjust office pic at smaller size

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
       <div className="bg-slate-900 w-screen pt-[64px]">
         <div className="h-[1px] ">
         </div>
-        <div className="lg:text-6xl md:text-5xl text-3xl font-black m-10">
+        <div className="lg:text-6xl md:text-5xl text-3xl font-black mx-10">
           Proudly <span className="animate-pulse text-amber-500">cracking necks and cashing checks</span> in the North Adams community for over 25 years!
           <p className="md:text-xl text-base md:p-10 py-10">
             In my 36+ years as a chiropractor I have helped 1000&#39;s of patients in our community
@@ -15,12 +15,12 @@ const About = () => {
             And, if you are interested, I will go beyond that and give you vitally important information,
             instruction, and advice on how to improve your overall health.
           </p>
+        </div>
           <img 
             src="/images/office_cover_photo.jpeg" 
             alt="office_photo" 
-            className="lg:px-10"  
+            className="sm:px-10 px-3"  
           />
-        </div>
         <div className="h-20">
         </div>
       </div>
