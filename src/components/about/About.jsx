fb78ee25ca2501769ebd89e78e16fc73acd70e3b
maxmay94 <maxmay94@gmail.com>
2022-12-16 12:17:16 -0500

    clean things up

---
+++ src/components/about/About.jsx
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <>
+      <div className="bg-slate-900 w-screen pt-[64px]">
+        <div className="h-[1px] ">
+        </div>
+        <div className="lg:text-6xl md:text-5xl text-3xl font-black m-10">
+          Proudly <span className="animate-pulse text-amber-500">cracking necks and cashing checks</span> in the North Adams community for over 25 years!
+          <p className="md:text-xl text-base md:p-10 py-10">
+            In my 36+ years as a chiropractor I have helped 1000&#39;s of patients in our community
+            with problems ranging from routine neck and back pain, to “pinched nerves”, headaches, a
+            variety of arm-shoulder-wrist pain and hip-knee-ankle-foot pain, and more. Every
+            complaint is unique to the individual. I will do everything I can to help you. Most importantly, I
+            will give you excellent advice and home instructions on how to help yourself get better faster.
+            And, if you are interested, I will go beyond that and give you vitally important information,
+            instruction, and advice on how to improve your overall health.
+          </p>
+          <img 
+            src="/images/office_cover_photo.jpeg" 
+            alt="office_photo" 
+            className="lg:px-10"  
+          />
+        </div>
+        <div className="h-20">
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default About
---
+++ src/components/info/Info.jsx
@@ -0,0 +1,36 @@
+const Info = () => {
+  return (
+    <>
+      <div className="sm:bg-fixed bg-cover bg-center bg-x-ray-temp h-screen w-screen pt-[64px]">
+        <div className="h-[1px]">
+        </div>
+        <div className="bg-slate-900/20 backdrop-blur-[3px] backdrop-grayscale p-3 m-10 w-fit contrast-200">
+          <div className="flex">
+            <p className=" grow font-black lg:text-6xl md:text-5xl sm:text-4xl text-2xl">
+              LET ME <br />
+              CRACK <br />
+              YOU UP.
+            </p>
+            <div className="px-3">
+              <img
+                className="rounded-full md:h-40 h-24 drop-shadow-md contrast-[.6]"
+                src="/images/dad_recent.jpeg" 
+                alt="peter_may" 
+              />
+          </div>
+          </div>
+          <p className="py-5 pr-5 font-bold w-2/3">
+            Book an appointment today! <br /> 
+            You can reach me at <br />
+            <span className="inline-block text-orange-500">(413) 664-9050</span> or 
+            <span className="text-orange-500"> docmay@gmail.com</span> <br />
+            My office conveniently is located at
+            73 Church Street in downtown North Adams
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Info
