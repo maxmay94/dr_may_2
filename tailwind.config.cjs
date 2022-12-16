593239ce0e4b18642853000063b79ce5e1cd8c9d
maxmay94 <maxmay94@gmail.com>
2022-12-16 13:55:51 -0500

    cleanup, delete old photos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,10 +9,8 @@ module.exports = {
       fontFamily: {
       },
       backgroundImage: {
-        'happy-people-temp' : "url('../images/happy_people_temp.jpeg')",
-        'jumping-woman' : "url('../images/jumping_woman.jpeg')",
         'people-running' : "url('../images/people_running.jpeg')",
-        'x-ray-temp' : "url('../images/x-ray_film_temp.jpeg')",
+        'x-ray' : "url('../images/x-ray_film.jpeg')",
       }
     },
   },
