eab5e28a47d1fab26e52480192331db9ada83048
maxmay94 <maxmay94@gmail.com>
2022-12-16 13:32:20 -0500

    clean up FAQ with components

---
+++ src/components/answer/Answer.jsx
@@ -0,0 +1,23 @@
+const Answer = ({answer, note}) => {
+  let dr_pic = "/images/dad_recent.jpeg"
+  return (
+    <>
+      <div className="flex mt-2">
+        <div className="bg-slate-300 text-teal-900 m-1 my-auto p-2 drop-shadow rounded">
+          { note &&
+            <div className="text-orange-700">
+              Note for Medicare patients:
+            </div>
+          }
+          {answer}
+        </div>
+        <img 
+          className="rounded-full ml-5 md:w-20 md:h-20 h-12 drop-shadow" 
+          src={dr_pic}
+          alt="dr_peter_may"/>
+      </div>
+    </>
+  )
+}
+
+export default Answer
---
+++ src/components/question/Question.jsx
@@ -0,0 +1,19 @@
+const Question = ({question}) => {
+  let questionPic = "/images/question.jpeg"
+  return (
+    <>
+      <div className="flex">
+        <img 
+          className="rounded-full mr-5 md:w-20 md:h-20 h-12 drop-shadow" 
+          src={questionPic}
+          alt="question" 
+        />
+        <div className="bg-slate-900 text-gray-300 m-1 my-auto p-2 drop-shadow rounded">
+          {question}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Question
